2fa12ea07a03f54d0c964541cebe4e740f4d6f7e
Hajar Choufih <hajarchoufih@gmail.com>
2024-12-22 18:04:34 +0100

    test2

--- app/src/main/java/com/example/e_commerce_app/MainActivity.java
+++ app/src/main/java/com/example/e_commerce_app/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
             //tajriba tajriba ghariba w papapapapapapapapaa
+            //waa fatiha
         });
     }
 }
