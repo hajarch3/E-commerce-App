b684383fd307d7e6ee69349bb4ac4268b4fc79eb
KAWTHAR2202 <kawtharlaafou@gmail.com>
2024-12-23 15:35:06 +0100

    test allah allah

--- app/src/main/java/com/example/e_commerce_app/MainActivity.java
+++ app/src/main/java/com/example/e_commerce_app/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             //waa fatiha
             // wa test num 2
             // ou lhindi gha wliyaaaa
+            // wa l3ariday day day
         });
     }
 }
